d9432633d4cc389fca8b7d66b576cdc7ffbde1dd
Determined-Hero <32328666+Determined-Hero@users.noreply.github.com>
2018-03-21 22:05:56 +0100

    Mall och Struktur WU2_LeGr
    Co-Authored-By: Victor Bohmies <victor.bohmies@gmail.com>

---
+++  index.php
@@ -0,0 +1,27 @@
++<?php
+ +echo "hi";
+ +//Including functions
+ +require ("resources/functions/functions.php");
+ +//Create function for the Subpages
+ +/*
+ +- Home [Startsidan]
+ ++- Läsa inlägg som artiklar
+ +++- Gå in på dessa inlägg
+ +- About
+ ++- Information om företaget
+ +- Posts (Bara inloggade kan se detta)
+ ++- Här lägger man ut sina inlägg.
+ ++- Redigera sina inlägg
+ ++- De andra användarnas inlägg.
+ ++- Gilla de andra användarnas inlägg
+ +- Profile
+ ++- Redigera din profils information.
+ +++- Namn, Profilbild, E-post adress, Telefonnummer osv.
+ ++- Sin profils information
+ +*/
+ +// if (){
+ +
+ +// }
+ +
+ +include ("resources/views/layout.php");
+ + ?>
