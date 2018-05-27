3071e78d487c3166c3b044d263e18879728f455e
Determined-Hero <32328666+Determined-Hero@users.noreply.github.com>
2018-05-27 19:35:06 +0200

    css o mallar
    css o mallar
    
    Co-Authored-By: Victor Bohmies <victor.bohmies@gmail.com>

---  index.php
+++  index.php
@@ -1,27 +1,31 @@
-+<?php
- +echo "hi";
- +//Including functions
- +require ("resources/functions/functions.php");
- +//Create function for the Subpages
- +/*
- +- Home [Startsidan]
- ++- Läsa inlägg som artiklar
- +++- Gå in på dessa inlägg
- +- About
- ++- Information om företaget
- +- Posts (Bara inloggade kan se detta)
- ++- Här lägger man ut sina inlägg.
- ++- Redigera sina inlägg
- ++- De andra användarnas inlägg.
- ++- Gilla de andra användarnas inlägg
- +- Profile
- ++- Redigera din profils information.
- +++- Namn, Profilbild, E-post adress, Telefonnummer osv.
- ++- Sin profils information
- +*/
- +// if (){
- +
- +// }
- +
- +include ("resources/views/layout.php");
- + ?>
+<?php
+ echo "hi";
+ //Including functions
+ require ("resources/functions/functions.php");
+ //Create function for the Subpages
+ /*
+ - Home [Startsidan]
+ - Läsa andra användarnas inlägg som artiklar
+ - Gå in på dessa inlägg
+ - Gilla dessa inlägg
+ - About
+ - Information om företaget
+ - Contact
+ - Kontaktuppgifter och skicka meddelanden
+ - Posts (Bara inloggade kan se detta)
+ - Läsa sina inlägg som artiklar
+ - Gå in på dessa inlägg
+ - Create
+ - Här lägger man ut sina inlägg.
+ - Redigera sina inlägg
+ - My Profile (Bara när du är inloggad kan du se detta)
+ - Redigera din profils information.
+ - Namn, Profilbild, E-post adress, Telefonnummer osv.
+ - Sin profils information
+ */
+ // if (){
+
+ // }
+
+ include ("resources/views/layout.php");
+  ?>
